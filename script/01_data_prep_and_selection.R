c58763388dc6033e3b4b8f1c61a5a42aefc95754
Pascal <pascal.marcel1117@gmail.com>
2026-08-25 21:11:20 +0200

    adjustment of section 5.2, removed one EDA plot, since it occured twice.

--- script/01_data_prep_and_selection.R
+++ script/01_data_prep_and_selection.R
@@ -144,8 +144,7 @@ print(continuous_summary)
 #   should be transformed (e.g., using polynomials) or discretized before entering the GLM.
 
 
-png("output/figures/gam_continuous_predictors.png", width = 2600, height = 1200, res = 300)
-png("output/figures/gam_continous_predictors.png", width = 1000, height = 800, res = 300)
+png("output/figures/gam_continuous_predictors.png", width = 2450, height = 1200, res = 300)
 # 2. Set global graphical parameters for a modern look
 # mfrow = c(1, 2) creates a 1x2 grid (side-by-side)
 # bty = "l" removes the top and right box borders for a cleaner look
